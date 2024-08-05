9f9a9593f490f8a20e0612af3092bbf9cd19cd05
George Jing <georgejing93@gmail.com>
2024-08-05 11:51:18 -0700

    fixed linter issue

--- pyxitter/xits/admin.py
+++ pyxitter/xits/admin.py
@@ -1,3 +1,15 @@
+from allauth.account.decorators import secure_admin_login
+from django.conf import settings
 from django.contrib import admin
 
-# Register your models here.
+from pyxitter.xits import models
+
+if settings.DJANGO_ADMIN_FORCE_ALLAUTH:
+    # Force the `admin` sign in process to go through the `django-allauth` workflow:
+    # https://docs.allauth.org/en/latest/common/admin.html#admin
+    admin.autodiscover()
+    admin.site.login = secure_admin_login(admin.site.login)  # type: ignore[method-assign]
+
+
+class XitAdmin(models.Xit):
+    pass
