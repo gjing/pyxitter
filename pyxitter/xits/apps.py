bbba1b4c8720f2bd79b2b0ad0c16358efdda53b2
George Jing <georgejing93@gmail.com>
2024-08-05 11:35:19 -0700

    migrations for xits

--- pyxitter/xits/apps.py
+++ pyxitter/xits/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class XitsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'xits'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "pyxitter.xits"
+    verbose_name = _("Xits")
