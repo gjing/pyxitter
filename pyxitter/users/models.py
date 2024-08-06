f008efc56b7bb2e3f0281e046f6679abe3667f76
George Jing <georgejing93@gmail.com>
2024-08-06 13:11:24 -0700

    followers and following

--- pyxitter/users/models.py
+++ pyxitter/users/models.py
@@ -1,5 +1,8 @@
+import random
+import string
+
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import CharField, ManyToManyField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -10,10 +13,25 @@ class User(AbstractUser):
     If adding fields that need to be filled at user signup,
     check forms.SignupForm and forms.SocialSignupForms accordingly.
     """
+    def generate_handle():
+        return "".join(random.SystemRandom().choice(
+            string.ascii_uppercase + string.digits  # noqa: COM812
+        ) for _ in range(32))
 
     # First and last name do not cover name patterns around the globe
     name = CharField(_("Name of User"), blank=True, max_length=255)
-    handle = CharField(_("Handle of User"), blank=True, max_length=255)
+    handle = CharField(
+        _("Handle of User"),
+        blank=False,
+        max_length=64,
+        unique=True,
+        default=generate_handle,
+    )
+    followers = ManyToManyField(
+        "self",
+        symmetrical=False,
+        related_name="following",
+    )
     first_name = None  # type: ignore[assignment]
     last_name = None  # type: ignore[assignment]
 
